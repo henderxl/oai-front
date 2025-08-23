5c576cf7ce0d16bf591d8a99e2119313e4e1e841
Henderson Ramírez <55466678+henderxl@users.noreply.github.com>
2025-08-23 00:23:48 -0500

    Replace logo with Sura logo

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -4,7 +4,6 @@ import { Dialog, Stack, TextField } from '@fluentui/react'
 import { CopyRegular } from '@fluentui/react-icons'
 
 import { CosmosDBStatus } from '../../api'
-import Contoso from '../../assets/Contoso.svg'
 import { HistoryButton, ShareButton } from '../../components/common/Button'
 import { AppStateContext } from '../../state/AppProvider'
 
@@ -42,7 +41,7 @@ const Layout = () => {
 
   useEffect(() => {
     if (!appStateContext?.state.isLoading) {
-      setLogo(ui?.logo || Contoso)
+      setLogo(ui?.logo || '/logo_sura.png')
     }
   }, [appStateContext?.state.isLoading])
 
