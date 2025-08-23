9dc5539d6a137bf76fe502a07fb6f68791a0b148
Henderson Ramírez <55466678+henderxl@users.noreply.github.com>
2025-08-23 00:24:02 -0500

    Updated Layout.tsx

--- src/pages/chat/Chat.tsx
+++ src/pages/chat/Chat.tsx
@@ -12,6 +12,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { nord } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 import styles from './Chat.module.css'
+import Contoso from '../../assets/Contoso.svg'
 import { XSSAllowTags } from '../../constants/sanatizeAllowables'
 
 import {
@@ -107,7 +108,7 @@ const Chat = () => {
 
   useEffect(() => {
     if (!appStateContext?.state.isLoading) {
-      setLogo(ui?.chat_logo || ui?.logo || '/logo_sura.png')
+      setLogo(ui?.chat_logo || ui?.logo || Contoso)
     }
   }, [appStateContext?.state.isLoading])
 
--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -4,6 +4,7 @@ import { Dialog, Stack, TextField } from '@fluentui/react'
 import { CopyRegular } from '@fluentui/react-icons'
 
 import { CosmosDBStatus } from '../../api'
+import Contoso from '../../assets/Contoso.svg'
 import { HistoryButton, ShareButton } from '../../components/common/Button'
 import { AppStateContext } from '../../state/AppProvider'
 
@@ -41,7 +42,7 @@ const Layout = () => {
 
   useEffect(() => {
     if (!appStateContext?.state.isLoading) {
-      setLogo(ui?.logo || '/logo_sura.png')
+      setLogo(ui?.logo || Contoso)
     }
   }, [appStateContext?.state.isLoading])
 
